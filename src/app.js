eec6a537ec8d2e115e83d2190884dc184dd557f4
Mustafa Teyfik AVKAN <teyfik.avkan@ict.btk.gov.tr>
2022-02-28 16:51:39 +0300

    done

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const app = Vue.createApp({
         return {
             common: {
                 mainTitle: "To Do List App",
-                userSelectionTitle: "User List",
             },
             user: {
-                userList: []
+                userList: [],
+                selectedUserId: "",
+                userListText: "User List",
+                deselectSelectedUserText: "Deselect User"
             },
             task: {
-                addTask: "Add Task",
-                deleteTask: "Delete",
+                addTaskText: "Add Task",
+                deleteTaskText: "Delete",
+                completedTaskCountText: "Completed Task Count:",
+                uncompletedTaskCountText: "Uncompleted Task Count:",
                 taskDescriptionPlaceHolder: "Task Description",
                 taskDescription: "",
                 taskList: [],
@@ -20,21 +24,58 @@ const app = Vue.createApp({
     computed: {
         completedTextDecoration() {
             return { 'textDecoration': true }
+        },
+        countCompletedTask() {
+            if (this.task.taskList.length !== 0) {
+
+                return this.task.taskList.filter(task => task.isDone).length;
+            }
+
+            return 0;
+        },
+        countUncompletedTask() {
+            if (this.task.taskList.length !== 0) {
+
+                return this.task.taskList.filter(task => !task.isDone).length;
+            }
+
+            return 0;
+        },
+        getSelectedUserName() {
+            console.log("asd");
+            if (this.user.selectedUserId && this.user.selectedUserId !== "") {
+                return this.user.userList.find(u => u.id === this.user.selectedUserId).userName;
+            }
+
+            return this.user.userListText
         }
     },
     methods: {
         isThereAnyUser() {
             return this.user.userList.length !== 0;
         },
+        onDeselectSelectedUser(event) {
+            event.stopPropagation();
+            this.user.selectedUserId = "";
+        },
+        onClickUserListDropdown(item) {
+            this.user.selectedUserId = item.id;
+            console.log(item);
+        },
         onChangeTaskEvent: function (event) {
             this.task.taskDescription = event.target.value;
         },
         onAddTaskEvent() {
             if (this.task.taskDescription && this.task.taskDescription !== "") {
-                this.task.taskList.push({ isDone: false, taskDescription: this.task.taskDescription });
-                this.task.taskDescription = "";
+                if (!this.user.selectedUserId || this.user.selectedUserId === "") {
+                    alert("You must choose a user!");
+                } else {
+
+                    this.task.taskList.push({ isDone: false, taskDescription: this.task.taskDescription, userId: this.user.selectedUserId });
+                    this.task.taskDescription = "";
+                }
             } else {
-                alert("task description cannot be at the moment.");
+                alert("Task description cannot be at the moment.");
             }
         },
         onDeleteTaskEvent(index) {
@@ -55,7 +96,7 @@ const app = Vue.createApp({
                 const user = {
                     "userList": result.data.userList,
                 };
-                this.user = user;
+                this.user = {...this.user, ...user};
             })
             .catch(error => {
                 console.error(error)
